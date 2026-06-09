654fe920ed1bedc5dc3e23303378f925e2aca2c7
JCalderonUnir <jhonnatan.calder180@comunidadunir.net>
2026-06-08 20:13:53 -0500

    Ajustes test ejecucion

--- src/test/java/ban/client/ClientServiceApplicationTests.java
+++ src/test/java/ban/client/ClientServiceApplicationTests.java
@@ -3,9 +3,11 @@ package ban.client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 class ClientServiceApplicationTests {
 
 	@Test
